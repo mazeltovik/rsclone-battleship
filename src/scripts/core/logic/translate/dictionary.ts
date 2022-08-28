3abe2276c223301e4953a70e452d070de719f72f
agtugchik <agtugchik13@gmail.com>
2022-08-28 16:41:34 +0300

    feat: add translate for registration pop up

--- src/scripts/core/logic/translate/dictionary.ts
+++ src/scripts/core/logic/translate/dictionary.ts
@@ -7,6 +7,10 @@ const dictionary: { [key: string]: { [key: string]: string } } = {
         warning: 'Do you really want to leave?<br> All progress will be lost!',
         yes: 'Yes',
         no: 'No',
+        password: 'Password',
+        username: 'Username',
+        login: 'Log In',
+        registration: 'Registration',
     },
     ru: {
         menu: 'Меню',
@@ -16,6 +20,10 @@ const dictionary: { [key: string]: { [key: string]: string } } = {
         warning: 'Вы хотите выйти?<br> Прогресс будет потерян!',
         yes: 'Да',
         no: 'Нет',
+        password: 'Пароль',
+        username: 'Имя',
+        login: 'Вход',
+        registration: 'Регистрация',
     },
 };
 
