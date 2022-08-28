51b613bb8fee2a1be4fc1a58d060fe8d5af539b0
AMS2302 <ams23post@gmail.com>
2022-08-28 03:35:56 +0300

    feat: add socket connection functionality in multiplayer

--- src/scripts/core/bin/multiplayerGame.ts
+++ src/scripts/core/bin/multiplayerGame.ts
@@ -1,18 +1,48 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 export default class MultiplayerGame {
+    socket: Socket;
+    playerNumber: number;
+    isReady: boolean;
+    isEnemyReady: boolean;
+    isShipsPlaced: boolean;
+    currentPlayer: string;
+
+    constructor() {
+        this.socket = io('http://localhost:3000/');
+        this.currentPlayer = 'user';
+        this.playerNumber = 0;
+        this.isReady = false;
+        this.isEnemyReady = false;
+        this.isShipsPlaced = false;
+        // this.shotFired = -1;
+    }
+
+    getPlayerNumber() {
+        this.socket.on('player-number', (num: string) => {
+            const number = parseInt(num);
+            if (number === -1) {
+                console.log('Server is full'); //todo: решить как реализовать на странице
+            } else {
+                this.playerNumber = number;
+                if (this.playerNumber === 1) this.currentPlayer = 'enemy';
+
+                console.log(this.playerNumber);
+            }
+        });
+    }
+
+    controlPlayerConnection(string: string) {
+        console.log(`Player ${string}`); //todo: решить как реализовать на странице
+    }
+
     start() {
         console.log('Multiplayer is started');
 
-        const socket = io('http://localhost:3000/');
+        this.getPlayerNumber();
 
-        socket.on('connect', function () {
-            console.log('...connect');
+        this.socket.on('player-connection', (string: string) => {
+            this.controlPlayerConnection(string);
         });
-        socket.on('disconnect', function () {
-            console.log('...disconnect');
-        });
-
-
     }
 }
