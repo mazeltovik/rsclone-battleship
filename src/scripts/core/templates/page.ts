d8a245b46e0421e1526718aec536ae01606f263d
agtugchik <agtugchik13@gmail.com>
2022-08-28 19:23:11 +0300

    feat: hide authorization button for game mode

--- src/scripts/core/templates/page.ts
+++ src/scripts/core/templates/page.ts
@@ -17,16 +17,19 @@ abstract class Page {
         const headerContainerMenu = <Element>document.querySelector('.header-container__menu');
         const currentPage = <Element>document.querySelector('#current-page');
         const burger = <Element>document.querySelector('.header-container__burger');
+        const authorization = <Element>document.querySelector('.header-container__authorization');
         if (type === 'add') {
             headerContainer.classList.add('header-container_game-mode');
             headerContainerMenu.classList.add('header-container__menu_game-mode');
             currentPage.classList.add('current-page_game-mode');
             burger.classList.add('header-container__burger_game-mode');
+            authorization.classList.add('header-container__authorization_game-mode');
         } else if (type === 'remove') {
             headerContainer.classList.remove('header-container_game-mode');
             headerContainerMenu.classList.remove('header-container__menu_game-mode');
             currentPage.classList.remove('current-page_game-mode');
             burger.classList.remove('header-container__burger_game-mode');
+            authorization.classList.remove('header-container__authorization_game-mode');
         }
     }
 
